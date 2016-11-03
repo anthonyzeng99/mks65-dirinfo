c9d3d5fe2194a3fd908248de54de98e22480cbe9
anthonyzeng99 <anthonyzeng99@gmail.com>
2016-11-03 01:13:40 -0400

    finished

--- dirinfo.c
+++ dirinfo.c
@@ -6,28 +6,43 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-DIR * open_dir(char *dir_to_scan) {
-  DIR *dir;
-  
-  dir = opendir(dir_to_scan);
+int scan_dir(char *dir_to_scan) {
+  DIR *dir = opendir(dir_to_scan);
   if (!dir) {
-    return NULL; //dir not found
+    return -1; //dir not found
   }
   
-  closedir(dir);
-  return dir;
-}
-
-void scan_dir(DIR *dir) {
   struct dirent *dir_info;
-  struct stat file;
+  struct stat *file;
+  int total_size = 0;
   while ((dir_info = readdir(dir))) {
-    printf("%s\n", dir_info -> d_name);
+    stat(dir_info -> d_name, file);
+    total_size += file -> st_size;
   }
+  printf("Statistics for directory: %s\n", dir_to_scan);
+  printf("Total Directory Size: %d bytes\n", total_size);
+
+
+  printf("\nDirectories:\n");
+  rewinddir(dir);
+  while ((dir_info = readdir(dir))) {
+    if (dir_info -> d_type == DT_DIR) {
+      printf("%s\n", dir_info -> d_name);
+    }
+  }
+
+  printf("\nRegular Files:\n");
+  rewinddir(dir);
+  while ((dir_info = readdir(dir))) {
+    if (dir_info -> d_type == DT_REG) {
+      printf("%s\n", dir_info -> d_name);
+    }
+  }
+
+  closedir(dir);
+  return 0;
 }
 
-int main() {
-  DIR *dir = open_dir("test_dir");
-  scan_dir(dir);
+int main() {≈
+  scan_dir("test_dir");
 }
